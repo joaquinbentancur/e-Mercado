e534c19946edf441e7e213c91400e3b99afd5dd0
Joaquin Bentancur <110427338+joaquinbentancur@users.noreply.github.com>
2022-09-03 22:07:52 +0900

    Reordenado
    Reordené todo de forma que me parece más coherente y ajusté unas variables que escribí mal.

--- js/products.js
+++ js/products.js
@@ -1,6 +1,5 @@
 let listaProductosActual = []; /* Ajusté el nombre de la variable en todo el código */
 let nombreCategoria = "" /* Agregué esta variable para ajustar el nombre de la categoría del título */
-let productCATID = "" /* Declaré esta variable para concatenar el json con el catID del local storage */
 const ORDEN_ASC_POR_PRECIO = "09";
 const ORDEN_DESC_POR_PRECIO = "90";
 const ORDEN_POR_PROD_CANT = "Cant.";
@@ -11,30 +10,17 @@ let busqueda = undefined; /* Para la barra buscadora */
 
 
 document.addEventListener("DOMContentLoaded", function () {
-    productCATID = PRODUCTS_URL + window.localStorage.getItem("catID") + EXT_TYPE;
+    let productCATID = PRODUCTS_URL + window.localStorage.getItem("catID") + EXT_TYPE;
+    /* Declaré esta variable para concatenar el json con el catID del local storage */
     getJSONData(productCATID).then(resultado => {
         if (resultado.status == "ok") {
             listaProductosActual = resultado.data.products;
             nombreCategoria = resultado.data.catName;
-            document.getElementById("categoria-mostrada").innerHTML += " " + nombreCategoria; /* Agregué id para mostrar en título */;
+            document.getElementById("categoria-mostrada").innerHTML += " " + nombreCategoria + "."; /* Agregué id para mostrar en título */;
 
             showProductsList()
         }
     })
-
-    /* Agrego tres eventos de click con la función de ordenar más el parámetro como criterio */
-    document.getElementById("sortAsc").addEventListener("click", function () {
-        ordernarMostrarProductos(ORDEN_ASC_POR_PRECIO);
-    });
-
-    document.getElementById("sortDesc").addEventListener("click", function () {
-        ordernarMostrarProductos(ORDEN_DESC_POR_PRECIO);
-    });
-
-    document.getElementById("sortByCount").addEventListener("click", function () {
-        ordernarMostrarProductos(ORDEN_POR_PROD_CANT);
-    });
-
 
     document.getElementById("rangeFilterPrice").addEventListener("click", function () { /* Obtengo valores min y max al filtrar */
         minPrice = document.getElementById("rangeFilterPriceMin").value;
@@ -57,15 +43,27 @@ document.addEventListener("DOMContentLoaded", function () {
         showProductsList();
     });
 
-
     document.getElementById("clearRangeFilter").addEventListener("click", function () { /* Limpiar */
         document.getElementById("rangeFilterPriceMin").value = "";
         document.getElementById("rangeFilterPriceMax").value = "";
-        /* Se encarga de reiniciar los inputs y pone undefined a las variables de mín. y máxm. */
+        /* Se encarga de reiniciar los inputs y pone undefined a las variables de mín. y máx. */
         minPrice = undefined;
         maxPrice = undefined;
 
         showProductsList();
+    });
+
+    /* Agrego tres eventos de click con la función de ordenar más el parámetro como criterio */
+    document.getElementById("sortAsc").addEventListener("click", function () {
+        ordenarYMostrarProductos(ORDEN_ASC_POR_PRECIO);
+    });
+
+    document.getElementById("sortDesc").addEventListener("click", function () {
+        ordenarYMostrarProductos(ORDEN_DESC_POR_PRECIO);
+    });
+
+    document.getElementById("sortByCount").addEventListener("click", function () {
+        ordenarYMostrarProductos(ORDEN_POR_PROD_CANT);
     });
 
     document.getElementById("buscador").addEventListener("input", function () {
@@ -74,6 +72,18 @@ document.addEventListener("DOMContentLoaded", function () {
     }) /* Evento de escucha tipo input para establecer valor del buscador */
     
 })
+
+function ordenarYMostrarProductos(criterioOrden, listaProductos) {
+    criterioOrdenActual = criterioOrden;
+
+    if (listaProductos != undefined) {
+        listaProductosActual = listaProductos;
+    }
+
+    listaProductosActual = ordenarProductos(criterioOrdenActual, listaProductosActual);
+
+    showProductsList();
+}
 
 function ordenarProductos(criterio, lista) {
     let result = [];
@@ -91,7 +101,7 @@ function ordenarProductos(criterio, lista) {
         });
     } else if (criterio === ORDEN_POR_PROD_CANT) {
         result = lista.sort(function (a, b) {
-            let aCount = parseInt(a.soldCount); /* Cambio la propiedad name por cost */
+            let aCount = parseInt(a.soldCount); /* Cambio la propiedad productCount por soldCount */
             let bCount = parseInt(b.soldCount);
 
             if (aCount > bCount) { return -1; }
@@ -103,17 +113,6 @@ function ordenarProductos(criterio, lista) {
     return result;
 }
 
-function ordernarMostrarProductos(criterioOrden, listaProductos) {
-    criterioOrdenActual = criterioOrden;
-
-    if (listaProductos != undefined) {
-        criterioOrdenActual = listaProductos;
-    }
-
-    criterioOrdenActual = ordenarProductos(criterioOrdenActual, listaProductosActual);
-
-    showProductsList();
-}
 
 function showProductsList() {
     let htmlContentToAppend = "";
