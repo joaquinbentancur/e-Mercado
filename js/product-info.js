5a55471d278c5b3cc83d9f28830874bc918d64b7
Joaquin Bentancur <110427338+joaquinbentancur@users.noreply.github.com>
2022-09-24 13:55:20 +0900

    Ajustes nombres

--- js/product-info.js
+++ js/product-info.js
@@ -18,36 +18,36 @@ document.addEventListener("DOMContentLoaded", function () {
 
   /* Click para nuevo comentario */
   document.getElementById("send-new-commentary").addEventListener("click", function () {
-      let newCommentText = document.getElementById("new-commentary-text").value;
-      let valorEstrella = document.getElementById("number-of-stars").value;
+    let newCommentText = document.getElementById("new-commentary-text").value;
+    let valorEstrella = document.getElementById("number-of-stars").value;
 
-      if (newCommentText != "" && (valorEstrella != "" && valorEstrella > 0 && valorEstrella < 6)) {
-        /* Cargo la fecha y la hora del sistema*/
-        let today = new Date();
+    if (newCommentText != "" && (valorEstrella != "" && valorEstrella > 0 && valorEstrella < 6)) {
+      /* Cargo la fecha y la hora del sistema*/
+      let today = new Date();
 
-        let date = today.getDate();
-        let month = today.getMonth() + 1;
-        let year = today.getFullYear();
+      let date = today.getDate();
+      let month = today.getMonth() + 1;
+      let year = today.getFullYear();
 
-        let currentDate = `${year}-${month}-${date}`;
+      let currentDate = `${year}-${month}-${date}`;
 
-        let hours = addZeroLeft(today.getHours());
-        let minutes = addZeroLeft(today.getMinutes());
-        let seconds = addZeroLeft(today.getSeconds());
+      let hours = addZeroLeft(today.getHours());
+      let minutes = addZeroLeft(today.getMinutes());
+      let seconds = addZeroLeft(today.getSeconds());
 
-        let currentTime = `${hours}:${minutes}:${seconds}`;
+      let currentTime = `${hours}:${minutes}:${seconds}`;
 
-        let currentDateTime = currentDate + " " + currentTime; /* Variable con fecha y hora */
+      let currentDateTime = currentDate + " " + currentTime; /* Variable con fecha y hora */
 
-        let estrellaPositiva = '<span class="fa fa-star checked"></span>';
-        let estrellaVacia = '<span class="fa fa-star"></span>';
+      let estrellaPositiva = '<span class="fa fa-star checked"></span>';
+      let estrellaVacia = '<span class="fa fa-star"></span>';
 
-        document.getElementById("comentarios").innerHTML += `
+      document.getElementById("comentarios").innerHTML += `
         <div class="comment-container">
         <p><span id="comment-user">${window.localStorage.getItem("usuario-ingresado")}</span> - ${currentDateTime} - ${estrellaPositiva.repeat(valorEstrella) + estrellaVacia.repeat(5 - valorEstrella)}</p>
     <p>${newCommentText}</p></div>`;
-      }
-    });
+    }
+  });
 });
 
 /* Función para ajustar la hora */
@@ -61,7 +61,9 @@ function showProduct(product) {
   let productDesc = document.getElementById("product-desc");
   let productCat = document.getElementById("product-cat");
   let productSoldCount = document.getElementById("product-soldCount");
-  let productImg = document.getElementById("product-img");
+  let crslIndicators = document.getElementById("carousel-indicators");
+  let productImg = document.getElementById("product-img-crsl");
+  let productRltd = document.getElementById("product-related");
 
   productName.innerHTML = product.name;
   productCost.innerHTML = product.currency + " " + product.cost;
@@ -69,28 +71,51 @@ function showProduct(product) {
   productCat.innerHTML = product.category;
   productSoldCount.innerHTML = product.soldCount;
 
-  let imgToAppend = "";
-  for (let i = 0; i < product.images.length; i++) {
+  productImg.innerHTML = `<div class="carousel-item active">
+  <img src="${product.images[0]}" class="d-block w-100" alt="${product.name[0]}">
+</div>`;
+  for (let i = 1; i < product.images.length; i++) {
     let imagen = product.images[i];
-    imgToAppend += `<img class="imagen-producto" src="${imagen}">
-    `;
+    /* Hago el indicador adaptativo a la cantidad */
+    crslIndicators.innerHTML += `
+    <button type="button" data-bs-target="#carouselWithIndicators" data-bs-slide-to="${i}" 
+    aria-label="Slide ${i+1}"></button>`;
+    /* Agrego las otras imágenes a mostrar */
+    productImg.innerHTML += `<div class="carousel-item">
+    <img src="${imagen}" class="d-block w-100" alt="${product.name}">
+  </div>`;
   }
-  productImg.innerHTML = imgToAppend;
+
+  let rltdToAppend = "";
+  for (let i = 0; i < product.relatedProducts.length; i++) { /* Recorro los productos relacionados */
+    let prodRltd = product.relatedProducts[i];
+    rltdToAppend += `
+    <div onclick="setProdID(${prodRltd.id})" class="cursor-active">
+    <img class="imagen-producto" src="${prodRltd.image}"</img>
+    <p>${prodRltd.name}</p>
+    </div>
+      `;
+  }
+  productRltd.innerHTML = rltdToAppend;
+}
+
+function setProdID(id) {
+  localStorage.setItem("prodID", id); /* Agrego onclick a la función previa para que pueda llamar setProdID() */
+  window.location = "product-info.html"
 }
 
 function showComments(comments) {
   let commentsToAppend = "";
   let estrellaPositiva = '<span class="fa fa-star checked"></span>';
   let estrellaVacia = '<span class="fa fa-star"></span>';
+  
   for (let i = 0; i < comments.length; i++) {
     let comentario = comments[i];
     commentsToAppend += `<div class="comment-container">
-    <p><span id="comment-user">${comentario.user}</span> - ${
-      comentario.dateTime
-    } - ${
-      estrellaPositiva.repeat(comentario.score) +
+    <p><span id="comment-user">${comentario.user}</span> - ${comentario.dateTime
+      } - ${estrellaPositiva.repeat(comentario.score) +
       estrellaVacia.repeat(5 - comentario.score)
-    }</p>
+      }</p>
     <p>${comentario.description}</div>`;
   }
   document.getElementById("comentarios").innerHTML = commentsToAppend;
