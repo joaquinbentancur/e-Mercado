7c27d05eb3f66390edd226a170a93f1648f38de7
Joaquin Bentancur <110427338+joaquinbentancur@users.noreply.github.com>
2022-10-16 00:01:48 +0900

    Entrega 5 completa
    Encontré solución para el cálculo en tiempo real del input de value. También agregué un control gráfico de "Confirmar" en el formulario de envío.

--- js/cart.js
+++ js/cart.js
@@ -1,26 +1,28 @@
-let cartData = {};
+let cartData = [];
 
 document.addEventListener("DOMContentLoaded", function () {
-    userDropbarMenu();
-    let userCart = CART_INFO_URL + "25801" + EXT_TYPE
+  userDropbarMenu();
+  
+  let userCart = CART_INFO_URL + "25801" + EXT_TYPE
 
-    getJSONData(userCart).then((resultado) => {
-        if (resultado.status === "ok") {
-            let cartData = resultado.data;
+  getJSONData(userCart).then((resultado) => {
+    if (resultado.status === "ok") {
+      cartData = resultado.data.articles;
+      console.log(cartData);
 
-            showCart(cartData);
-        }
-    })
+      showCart(cartData);
+    }
+  })
 })
 
 function showCart(cartData) {
-    let listadoCarrito = document.getElementById("listadoCarrito");
-    let plantillaArticulo = "";
+  let listadoCarrito = document.getElementById("listadoCarrito");
+  let plantillaArticulo = "";
 
-    for (let i = 0; i < cartData.articles.length; i++) {
-        let prodCarrito = cartData.articles[i];
-        plantillaArticulo +=
-            `<div class="col">
+  for (let i = 0; i < cartData.length; i++) {
+    let prodCarrito = cartData[i];
+    plantillaArticulo +=
+      `<div class="col">
         <img src="${prodCarrito.image}" style="height: 50px;">
         </img>
           </div>
@@ -31,21 +33,21 @@ function showCart(cartData) {
             <p>${prodCarrito.currency} ${prodCarrito.unitCost}</p>
           </div>
           <div class="col">
-            <input onkeyup="getProductQty(${prodCarrito.id,prodCarrito.currency,prodCarrito.unitCost})" id="${prodCarrito.id}q" type="number" min="1" value="${prodCarrito.count}" class="cart-qty-input">
+            <input onchange="changeCount(${prodCarrito.id}, ${i})" id="${prodCarrito.id}q" type="number" min=1 value="${prodCarrito.count}" class="cart-qty-input">
           </div>
           <div class="col fw-bold">
-            <p id="${prodCarrito.id}s">${prodCarrito.currency} ${((prodCarrito.unitCost) * (prodCarrito.count))}</p>
+            <p>${prodCarrito.currency} ${((prodCarrito.unitCost) * (prodCarrito.count))}</p>
           </div>`
-    }
-    listadoCarrito.innerHTML = plantillaArticulo + "<hr>";
+  }
+  listadoCarrito.innerHTML = plantillaArticulo + "<hr>";
 }
 
-function getProductQty(prodId,prodCurrency,prodUnitCost) {
-    let idQ = prodId+"q";
-    let idS = prodId+"s";
-    console.log(idQ);
-    console.log(idS);
-    let productQty = document.getElementById(idQ); /* Le quité .value */
-    console.log(productQty);
-    document.getElementById(idS).innerHTML = (prodCurrency)((prodUnitCost) * productQty);
+function changeCount(prodId, prodPosition) {
+  let idQ = prodId + "q";
+  let inputQ = document.getElementById(idQ).value
+  if (inputQ >= 1){ /* Mínimo de 1 o vuelve al último value */
+    cartData[prodPosition].count = inputQ;
+  }  
+  
+  showCart(cartData)
 }
